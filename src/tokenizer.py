dca41e954e672799990a38d86895f0c4b8622a87
leand12 <leandrosilva12@ua.pt>
2021-11-12 15:14:28 +0000

    tokenizer contractions

--- src/tokenizer.py
+++ src/tokenizer.py
@@ -6,33 +6,40 @@ import re
 
 class Review:
     ID = 2
-    HEADLINE = 11
-    BODY = 12
+    HEADLINE = 12
+    BODY = 13
 
 class Tokenizer:
     
-    def __init__(self, min_length=3, case_folding=True, no_numbers=True, stopwords=True, stemmer=True):
+    def __init__(self, min_length=3, case_folding=True, no_numbers=True, stopwords=True, contractions=True, stemmer=True):
         self.min_length = min_length
         self.case_folding = case_folding
         self.no_numbers = no_numbers
-        self.stopwords = {}
+        self.contractions = {}
+        self.stopwords = set()
         self.stemmer = None
+        dirname, _ = os.path.split(os.path.abspath(__file__))
         if stopwords:
-            dirname, _ = os.path.split(os.path.abspath(__file__))
-            self.stopwords = {w for w in open(dirname + "/../data/nltk_en_stopwords.txt", "r").read().split()}
+            self.stopwords = {w.lower() for w in open(dirname + "/../data/nltk_en_stopwords.txt", "r").read().split()}
+        if contractions:
+            for line in open(dirname + "/../data/en_contractions.txt", "r").read().split():
+                token, term = line.lower().split(',')
+                self.contractions[token] = term
         if stemmer:
             self.stemmer = SnowballStemmer("english")
 
     def normalize_tokens(self, terms):
         # TODO: what to do with hiphens?
-        
-        terms = [re.sub(r'[^\w\s]','',term) for term in terms]
+        terms = [re.sub(r'[^\w\s]', ' ', term).split() for term in terms]
+
         if self.min_length:
             terms = [term for term in terms if len(term) >= self.min_length]
         if self.stopwords:
-            terms = [term for term in terms if term.lower() not in self.stopwords]
+            terms = [term for term in terms if term not in self.stopwords]
+        if self.contractions:
+            terms = [term if term not in self.contractions else self.contractions[term] for term in terms]
         if self.no_numbers:
-            terms = [term for term in terms if not term.replace(",", "").replace(".", "").isdigit()]
+            terms = [term for term in terms if not term.replace(',', '').replace('.', '').isdigit()]
         if self.case_folding:
             terms = [term.casefold() for term in terms]
         if self.stemmer:
@@ -42,7 +49,7 @@ class Tokenizer:
 
     def tokenize(self, line):
         doc = line.split('\t')
-        review = doc[Review.HEADLINE] + doc[Review.BODY]
+        review = doc[Review.HEADLINE] + " " + doc[Review.BODY]
         review_id = doc[Review.ID]
 
         #self.update_fields(doc)
